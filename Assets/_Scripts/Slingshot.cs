ce817e5f50e3daa272ca325d9f83be8a75df9303
Sentientprism <doughertyedmund7@gmail.com>
2025-09-25 14:02:21 -0400

    made a castle thing

--- Assets/_Scripts/Slingshot.cs
+++ Assets/_Scripts/Slingshot.cs
@@ -42,35 +42,37 @@ public class Slingshot : MonoBehaviour{
 
     projectile.GetComponent<Rigidbody>().isKinematic = true;
  }
- void Update(){
+    void Update() {
 
-    if (!aimingMode) return;
+        if (!aimingMode) return;
 
-    Vector3 mousePos2D = Input.mousePosition;
-    mousePos2D.z = -Camera.main.transform.position.z;
-    Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
-    Vector3 mouseDelta = mousePos3D -launchPos;
+        Vector3 mouseDelta = mousePos3D - launchPos;
 
-    float maxMagnitude = this.GetComponent<SphereCollider>().radius;
-    if (mouseDelta.magnitude > maxMagnitude){
-        mouseDelta.Normalize();
-        mouseDelta*= maxMagnitude;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude) {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            aimingMode = false;
+            Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+            projRB.isKinematic = false;
+            projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            projRB.linearVelocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            Instantiate<GameObject>(projLinePrefab, projectile.transform);
+            projectile = null;
+            MissionDemolition.SHOT_FIRED();
+        }
+        }
     }
-
-    Vector3 projPos = launchPos + mouseDelta;
-    projectile.transform.position = projPos;
-
-    if(Input.GetMouseButtonUp(0)){
-
-        aimingMode = false;
-        Rigidbody projRB = projectile.GetComponent<Rigidbody>();
-        projRB.isKinematic = false;
-        projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
-        projRB.linearVelocity = -mouseDelta * velocityMult;
-        FollowCam.POI = projectile;
-        Instantiate<GameObject>(projLinePrefab, projectile.transform);
-        projectile = null;
-    }
- }
-}
